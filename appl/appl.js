692fecfea1a1fd06e6c3c828f0823e4a9a68b04c
Gregory Pievski <gregpiev@gmail.com>
2017-07-03 11:28:27 +0300

    Stage 2

--- appl/appl.js
+++ appl/appl.js
@@ -1,7 +1,2 @@
 'use strict';
-var angular = require('angular');
-var uirouter = require("angular-ui-router");
-var ngRoute = require('angular-route');
-var ngSanitize = require('sanitize');
-
-let gameAppl = angular.module('gameAppl', ['ngRoute', 'uirouter', 'ngSanitize']);
+let gameAppl = angular.module('gameAppl', ['ngRoute', 'ui.router', 'ngSanitize', 'ngFileUpload']);
